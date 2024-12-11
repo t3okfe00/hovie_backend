3624ce90fd01041d6c3d49db07f63d9a45a1dcc2
lukasmarkpfister <151028796+lukasmarkpfister@users.noreply.github.com>
2024-12-11 02:16:26 +0200

    removing *

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ const app = express();
 // Use CORS middleware
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL, // Genau das Frontend, das Zugriff benötigt
     credentials: true, //
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type",
